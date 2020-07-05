5370cc21da3ae0e156f1271b5ecb1ba53166de8e
Yuhan Yang <55200109+yune-lilias@users.noreply.github.com>
2020-07-05 00:51:57 -0400

    Add files via upload

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,58 +1,42 @@
-CREATE DATABASE IF NOT EXISTS book_review;
-USE `book_review`;
+drop database if exists bookReviewRS;
+create database bookReviewRS;
+use bookReviewRS;
 
-CREATE TABLE IF NOT EXISTS user
-(
-  email VARCHAR(50) NOT NULL,
-  username VARCHAR(20) NOT NULL,
-  userID INT NOT NULL AUTO_INCREMENT,
-  password VARCHAR(20) NOT NULL,
-  nickname VARCHAR(20),
-  PRIMARY KEY (userID)
+create table if not exists USER(
+userID int not null auto_increment,
+username varchar(20),
+email varchar(100),
+password varchar(20),
+phone bigint(10),
+primary key (userID)
 );
 
-CREATE TABLE IF NOT EXISTS book
-(
-   isbn VARCHAR(14) NOT NULL,
-   title VARCHAR(50) NOT NULL,
-   author VARCHAR(40) NOT NULL,
-   PRIMARY KEY (isbn)
+create table if not exists BOOK(
+isbn bigint(13) not null,
+title varchar(100),
+author varchar(50),
+primary key(isbn)
 );
 
-CREATE TABLE IF NOT EXISTS favorite
-(
-   userID INT NOT NULL,
-   isbn1 VARCHAR(14),
-   isbn2 VARCHAR(14),
-   isbn3 VARCHAR(14),
-   isbn4 VARCHAR(14),
-   isbn5 VARCHAR(14),
-   FOREIGN KEY(userID) REFERENCES user(userID)
+create table if not exists WEBSITE(
+webName varchar(50),
+URL varchar(255),
+isbn bigint(13),
+foreign key (isbn) references BOOK(isbn)
 );
 
-CREATE TABLE IF NOT EXISTS history
-(
-   userID INT NOT NULL,
-   time BIGINT,
-   isbn VARCHAR(14),
-   FOREIGN KEY(userID) REFERENCES user(userID),
-   FOREIGN KEY(isbn) REFERENCES book(isbn)
+create table if not exists REVIEW(
+rating float(3),
+summary varchar(5000),
+isbn bigint(13),
+foreign key (isbn) references BOOK(isbn)
 );
 
-CREATE TABLE IF NOT EXISTS comment
-(
-   isbn VARCHAR(14) NOT NULL,
-   platformname VARCHAR(20) NOT NULL,
-   comment1 VARCHAR(1000),
-   rate1 FLOAT,
-   comment2 VARCHAR(1000),
-   rate2 FLOAT,
-   comment3 VARCHAR(1000),   
-   rate3 FLOAT,
-   rating FLOAT,
-   url VARCHAR(200),
-   FOREIGN KEY(isbn) REFERENCES book(isbn)  
+create table if not exists SEARCHISTORY(
+userID int,
+isbn bigint(13),
+favorite bool not null default 0,
+accessTime DATETIME NOT NULL DEFAULT current_timestamp,
+foreign key (userID) references USER(userID),
+foreign key (isbn) references BOOK(isbn)
 );
-
-
-
